94a360682f4dff602d83f2bb1daf7cf75c1ad879
ChirayuBhatia <120477374+ChirayuBhatia@users.noreply.github.com>
2025-01-27 11:58:33 +0530

    First Commit with Login and Registration View

--- PlacementPortal/urls.py
+++ PlacementPortal/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('apiv1.urls'), name='apiv1'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
